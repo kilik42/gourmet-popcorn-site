4e58b78d8ad4ddd0c8230fa767461083a2e4abd3
marvin evins <maevins@cps.edu>
2019-04-27 22:11:26 -0500

    added menu items

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function (){
+
+
+  //magnific popover
+    $('.parent-container').magnificPopup({
+   delegate: 'a', // child items selector, by clicking on it popup will open
+   type: 'image',
+
+  gallery:{
+    enabled: true
+  }
+
+   // other options
+  });
+
+
+});
