3c90eb5bd07da3211f11462c1242e04506ef50b1
marvin evins <maevins@cps.edu>
2019-06-04 22:37:00 -0500

    finished the scrolling effect

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 $(document).ready(function (){
+//toggler btn
+    $('.navbar-toggler').click(function(){
+      $('.navbar-toggler').toggleClass('change')
+
+    })
+
+
+//sticky navbar less padding
+$(window).scroll(function(){
+   let position = $(this).scrollTop();
+   if(position>=718){
+     $('.navbar').addClass('navbar-background');
+     $('.navbar').addClass('fixed-top');
+
+   }
+   else{
+     $('.navbar').removeClass('navbar-background');
+     $('.navbar').removeClass('fixed-top');
+   }
+})
+
+
+
 
 
   //magnific popover
